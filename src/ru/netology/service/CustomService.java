2a522209adce8c4f13faed91c8f19bd7e3e43268
RusinEA <earusin@gmail.com>
2024-09-26 12:58:40 +0300

    Update

---
+++ src/ru/netology/service/CustomService.java
@@ -0,0 +1,7 @@
+package ru.netology.service;
+
+public class CustomService {
+    public static double calculateCustoms(int price, int weight) {
+        return (0.01 * price) + weight * 100;
+    }
+}
