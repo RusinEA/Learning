4f4aab79b8e76c82b0e475d0ef2a0d4687bb1e66
RusinEA <earusin@gmail.com>
2024-10-31 10:35:21 +0300

    Update

--- src/ru/netology/service/Book.java
+++ src/ru/netology/service/Book.java
@@ -5,36 +5,30 @@ public class Book {
     public int releaseYear;
     public String author;
     public int pages;
-    public Book(String title, int releaseYear, String author, int pages){
+
+    public Book(String title, int releaseYear, String author, int pages) {
         this.title = title;
         this.releaseYear = releaseYear;
         this.author = author;
         this.pages = pages;
     }
 
-    public boolean isBig(){
-        if (pages > 500){
-            return true;
-        }
-        else {
-            return false;
-        }
+    public boolean isBig() {
+        return pages > 500;
     }
 
-    public boolean matches(String word){
+    public boolean matches(String word) {
         if (title.contains(word) || author.contains(word)) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public int estimatePrice(){
-        if (pages <=84){
+    public int estimatePrice() {
+        if (pages <= 84) {
             return 250;
-        }
-        else {
+        } else {
             return pages * 250;
         }
     }
