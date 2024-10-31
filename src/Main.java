f5805c556a297e5a75c5f56813c9b4d909252264
RusinEA <earusin@gmail.com>
2024-10-31 16:38:26 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,2 @@
-import ru.netology.service.Book;
-
 public class Main {
-    public static void main(String[] args) {
-        Book newBook = new Book("Solaris", 1961, "Lem", 210);
-        System.out.println("Большая ли книга");
-        System.out.println(newBook.isBig());
-        System.out.println("Содержится ли в описании книги (названии или в имени автора) переданное параметром слово");
-        System.out.println(newBook.matches("Lem"));
-        System.out.println("Оценка стоимости книги (целое число в рублях) по формуле 3 рубля за страницу");
-        System.out.println(newBook.estimatePrice());
-
-    }
 }
