cbafc6c0f9c4874a82326d9065ac87f9602591ad
RusinEA <earusin@gmail.com>
2024-11-15 09:57:25 +0300

    Update

--- src/NetologyHW7.java
+++ src/NetologyHW7.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
-
 import ru.netology.service.Basket;
 
-public class Main {
+import java.util.Scanner;
+
+public class NetologyHW7 {
     public static void main(String[] args) {
 
         String[] productsArr = {"Хлеб", "Яблоки", "Молоко", "Гречневая крупа"};
         int[] pricesArr = {14, 30, 50, 80};
         Scanner scanner = new Scanner(System.in);
-        int[] countProductsArr = new int[4];
+        int[] countProductsArr = new int [4];
         System.out.println("В нашей лавке сегодня такие продукты: ");
 
         for (int i = 0; i < productsArr.length; i++) {
             System.out.println("Наименование: " + productsArr[i] + " | Стоимость: " + pricesArr[i] + "р/шт");
         }
 
-        while (true) {
+        while (true){
 
             System.out.println("Выберите индекс продукта и его количество, или введите end для выхода из программы");
 
@@ -25,17 +25,17 @@ public class Main {
                 break;
             }
 
-            String[] parts = input.split(" ");
+            String [] parts = input.split(" ");
 
             int productNumber = Integer.parseInt(parts[0]);
             int productCount = Integer.parseInt(parts[1]);
 
-            countProductsArr[productNumber - 1] += productCount;
+            countProductsArr[productNumber-1] += productCount;
 
         }
 
         System.out.println("Ваша корзина: ");
-        for (int i = 0; i < productsArr.length; i++) {
+        for (int i = 0; i < productsArr.length; i++  ){
             Basket.basketOut(productsArr[i], countProductsArr[i], pricesArr[i]);
         }
 
--- src/ru/netology/service/Basket.java
+++ src/ru/netology/service/Basket.java
@@ -3,9 +3,9 @@ package ru.netology.service;
 public class Basket {
     public static void basketOut(String name, int count, int price) {
 
-       if (count >0) {
-           int summ = count*price;
-           System.out.println(name + " шт " + price + " руб/шт " + summ + " руб в сумме");
+        if (count > 0) {
+            int summ = count * price;
+            System.out.println(name + " шт " + price + " руб/шт " + summ + " руб в сумме");
         }
     }
 }
