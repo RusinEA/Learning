fc7229e3ba12ac83bf77dd38ca14d79e008e45b9
RusinEA <earusin@gmail.com>
2024-12-10 17:13:59 +0300

    Update

--- src/ru/netology/service/Account.java
+++ src/ru/netology/service/Account.java
@@ -2,31 +2,28 @@ package ru.netology.service;
 
 public abstract class Account {
 
-    protected long balance;
+    protected long balance = 0;
 
     public void setBalance(long balance) {
         this.balance = balance;
-
     }
 
-    long getBalance() {
+    protected long getBalance() {
         return balance;
     }
 
-    boolean add(long amount){
+    public abstract boolean add(long amount);
 
+    public abstract boolean pay(long amount);
+
+    public boolean transfer(Account account, long amount){
+        if (pay(amount)){
+            if (account.add(amount)){
+                return true;
+            }
+            add(amount);
+        }
         return false;
-    }
-
-    boolean pay(long amount){
-
-        return false;
-    }
-
-    boolean transfer(Account account, long amount){
-
-        return false;
-
     }
 
 
