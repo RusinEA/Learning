c11d39ec6c85f73281ef2d32f341443b36125e09
RusinEA <earusin@gmail.com>
2025-02-04 14:40:35 +0300

    Update

---
+++ src/Counter.java
@@ -0,0 +1,22 @@
+public class Counter {
+
+        public String name;
+        public int count;
+
+        public Counter(String name){
+            this(name, 0);
+        }
+
+        public Counter(String name, int count){
+            this.name = name;
+            this.count = count;
+        }
+        public int decrement(){
+            return -- count;
+        }
+
+        public int increment(){
+            return ++ count;
+        }
+
+}
