35fee4ec7f3290e7d92e9cbc263824b6d34c4c2d
RusinEA <earusin@gmail.com>
2025-09-10 15:03:54 +0300

    Learn commit

---
+++ src/ru/skillbox/Main.java
@@ -0,0 +1,11 @@
+package ru.skillbox;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
---
+++ src/ru/skillbox/Printer.java
@@ -0,0 +1,59 @@
+package ru.skillbox;
+
+public class Printer {
+
+    public static void main(String[] args) {
+        int xStep = 2;
+        int yStep = 3;
+        byte x = 5;
+        byte y = 10;
+        short z = (short) (x + y);
+        System.out.println(z);
+        byte s = (byte) 500;
+        System.out.println(s);
+// Создайте 3 переменных типа Frog и проинициализируйте их новыми объектами типа Frog. Заполните пропуски в коде:
+
+
+//        Frog frog1 = new Frog();
+//        Frog frog2 = new Frog();
+//        Frog frog3 = new Frog();
+
+        Frog[] frogs = new Frog[] {new Frog(), new Frog(), new Frog()};
+
+//        frogs[0] = frog1;
+//        frogs[1] = frog2;
+//        frogs[2] = frog3;
+        System.out.println(frogs.length);
+// Переместите каждую из созданных лягушек на случайную позицию влево и вверх и выведите ее положение на экран, вызвав метод print. Заполните пропуски в коде и повторите этот блок кода столько раз, сколько это необходимо:
+        int i = 0;
+
+        for (i = 0; i <= frogs.length - 1; i = i + 1) {
+            frogs[i].moveLeft(xStep);
+            frogs[i].moveUp(yStep);
+            frogs[i].print(i);
+        }
+
+
+    }
+    public static class Frog {
+        int xPosition;
+        int yPosition;
+        boolean isAlive = true;
+
+        void moveLeft(int offset) {
+            xPosition = xPosition - offset;
+        }
+
+        void moveUp(int offset) {
+            yPosition = yPosition + offset;
+        }
+
+        void print(int frogNumber) {
+            System.out.println("frogs["+ frogNumber +"]: "
+                    + "xPosition= " + xPosition
+                    + "; yPosition= " + yPosition);
+        }
+    }
+}
+
+
